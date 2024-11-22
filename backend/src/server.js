29fa90e51b983bd72c2f6a9a11110a335d50cd55
SoloScripter3 <vardhanv6259@gmail.com>
2024-11-22 20:20:18 +0530

    added route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const { connectDB } = require("./config/db");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
 connectDB();
+
+app.use(express.json());
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 

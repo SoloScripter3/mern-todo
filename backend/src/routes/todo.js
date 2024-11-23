bbe5296b22ef0cd858589bf5f093ed52b5a1a6f3
SoloScripter3 <vardhanv6259@gmail.com>
2024-11-23 15:58:55 +0530

    added update route

--- backend/src/routes/todo.js
+++ backend/src/routes/todo.js
@@ -25,3 +25,30 @@ router.post("/add", auth, async (req, res) => {
     return res.json({ message: "something went wrong" });
   }
 });
+
+//updating completed task
+router.put("/:id", async (req, res) => {
+  try {
+    const updates = {};
+    if (req.body.title !== undefined) {
+      updates.title = req.body.title;
+    }
+    if (req.body.completed !== undefined) {
+      updates.completed = req.body.completed;
+    }
+
+    const todo = await Todo.findOneAndUpdate(
+      { _id: req.params.id, user: req.user },
+      updates,
+      { new: true }
+    );
+
+    if (!todo) {
+      return res.json({ message: "Todo is not found" });
+    }
+
+    return res.json({ message: todo });
+  } catch (err) {
+    return res.json({ message: "error in updating todo" });
+  }
+});

cb6842d3eb9c6731ff2bd493f38811bc0cba7852
SoloScripter3 <vardhanv6259@gmail.com>
2024-11-23 09:07:27 +0530

    added authMiddleware

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  const authHeaders = req.headers["Authorization"];
+  const token = authHeaders.split(" ")[1];
+
+  if (!token) {
+    return res.json({ message: "Forbidden login" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = decoded.userId;
+    next();
+  } catch (err) {
+    return res.json({ message: "Invalid token" });
+  }
+};
+
+module.exports = authMiddleware;

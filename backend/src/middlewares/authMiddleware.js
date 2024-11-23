cc92dd4455dfde256036ec3ebfed3983ea95f86c
SoloScripter3 <vardhanv6259@gmail.com>
2024-11-23 17:14:54 +0530

    typo fix

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const authHeaders = req.headers["Authorization"];
+  const authHeaders = req.headers["authorization"];
   if (!authHeaders) {
     return res.json({ message: "Denied" });
   }

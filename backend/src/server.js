798471150401cea5d9bf5ed8eb7eec3518bd5183
SoloScripter3 <vardhanv6259@gmail.com>
2024-11-23 16:13:27 +0530

    added todoRoutes to sever file

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const { connectDB } = require("./config/db");
 const authRoutes = require("./routes/auth");
+const todoRoutes = require("./routes/todo");
 
 const app = express();
 
@@ -9,6 +10,7 @@ connectDB();
 
 app.use(express.json());
 app.use("/auth", authRoutes);
+app.use("/todo", todoRoutes);
 
 const PORT = process.env.PORT || 3000;
 
